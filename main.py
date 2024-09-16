c5fe0ce3dacc84f50b128ca2a3b397c8aba3ef01
Ryo <jukenohtani@gmail.com>
2024-09-16 15:27:33 +0900

    fix

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-@client.event
+@ client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     # バックグラウンドタスクを起動
